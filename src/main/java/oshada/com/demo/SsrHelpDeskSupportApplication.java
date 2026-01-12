d539291e327edc462fd00d88c06a7157e5a504cf
Oshadha <moshadha23@gmail.com>
2026-01-12 14:06:00 +0530

    initial commit

---
+++ src/main/java/oshada/com/demo/SsrHelpDeskSupportApplication.java
@@ -0,0 +1,14 @@
+package oshada.com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsrHelpDeskSupportApplication {
+
+	public static void main(String[] args) {
+		System.out.println("system is working");
+		SpringApplication.run(SsrHelpDeskSupportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oshada/com/demo/SsrHelpDeskSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package oshada.com.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsrHelpDeskSupportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
